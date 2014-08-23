fd05718301e18d49b4dd5533a8152b16773c8afe
BennyQBD <bennyqbdspam@gmail.com>
2014-08-23 17:01:59 -0400

    Reorganized Codebase

--- src/Game.java
+++ src/Game.java
@@ -6,22 +6,22 @@ public class Game
 	private final ArrayList<Entity>  m_entities;
 	private final ArrayList<Entity>  m_toRemove;
 
-	private final ArrayList<Player>  m_laserStarts;
-	private final ArrayList<Player>  m_laserEnds;
-	private final ArrayList<Player>  m_laserStartToRemove;
-	private final ArrayList<Player>  m_laserEndToRemove;
+//	private final ArrayList<Player>  m_laserStarts;
+//	private final ArrayList<Player>  m_laserEnds;
+//	private final ArrayList<Player>  m_laserStartToRemove;
+//	private final ArrayList<Player>  m_laserEndToRemove;
 	private final Stars3D            m_stars;
 	private float                    m_delta;
-	private final Entity             m_mainPlayer;
+	private final Player             m_mainPlayer;
 
 	public Game()
 	{
 		m_entities           = new ArrayList<Entity>();
 		m_toRemove           = new ArrayList<Entity>();
-		m_laserStarts        = new ArrayList<Player>();
-		m_laserEnds          = new ArrayList<Player>();
-		m_laserStartToRemove = new ArrayList<Player>();
-		m_laserEndToRemove   = new ArrayList<Player>();
+//		m_laserStarts        = new ArrayList<Player>();
+//		m_laserEnds          = new ArrayList<Player>();
+//		m_laserStartToRemove = new ArrayList<Player>();
+//		m_laserEndToRemove   = new ArrayList<Player>();
 		m_stars = new Stars3D(4096, 64.0f, 4.0f);
 
 		m_mainPlayer = new Player(true, -1.0f, -1.0f,
@@ -61,79 +61,53 @@ public class Game
 
 		for(int i = 0; i < m_entities.size(); i++)
 		{
-			for(int j = i + 1; j < m_entities.size(); j++)
+			if(m_entities.get(i) instanceof Player
+			  && m_entities.get(i) != m_mainPlayer)
 			{
-				Entity current = m_entities.get(i);
-				Entity other = m_entities.get(j);
+				Player other = (Player)m_entities.get(i);
+				Player result = m_mainPlayer
+					.CheckCollisionRecursive(other);
 
-				if(current instanceof Player &&
-				   other instanceof Player)
+				if(result != null)
 				{
-					Player currentPlayer = (Player)current;
-					Player otherPlayer   = (Player)other;
-
-					if(currentPlayer.GetIsActive() 
-					   && !otherPlayer.GetIsActive()
-					   && currentPlayer.SphereIntersect(otherPlayer))
-					{
-						m_laserStarts.add(currentPlayer);
-						m_laserEnds.add(otherPlayer);
-						otherPlayer.SetIsActive(true);
-					}
-					else if(otherPlayer.GetIsActive()
-					   && !currentPlayer.GetIsActive()
-					   && currentPlayer.SphereIntersect(otherPlayer))
-					{
-						m_laserStarts.add(otherPlayer);
-						m_laserEnds.add(currentPlayer);
-						currentPlayer.SetIsActive(true);
-					}
+					result.AddChild(other);
+					m_toRemove.add(other);
+					other.SetIsActive(true);
 				}
 			}
-		}
 
-		for(int i = 0; i < m_entities.size(); i++)
-		{
+			Player[] laserResult = new Player[2];
+
 			if(m_entities.get(i) instanceof 
 					DestroyableObject)
 			{
 				DestroyableObject current = 
 					(DestroyableObject)m_entities.get(i);
 
-				for(int j = 0; j < m_laserStarts.size();j++)
+				m_mainPlayer.CheckLaserCollision(current,
+						laserResult);
+
+				if(laserResult[0] != null)
 				{
-					Entity start = m_laserStarts.get(j);
-					Entity end   = m_laserEnds.get(j);
-
-					if(current.LineIntersect(
-						start.GetX(), start.GetY(), 
-						end.GetX(), end.GetY()))
-					{
-						m_toRemove.add(current);
-					}
+					m_toRemove.add(current);
 				}
+
+				laserResult[0] = null;
+				laserResult[1] = null;
+//
+//				for(int j = 0; j < m_laserStarts.size();j++)
+//				{
+//					Entity start = m_laserStarts.get(j);
+//					Entity end   = m_laserEnds.get(j);
+//
+//					if(current.LineIntersect(
+//						start.GetX(), start.GetY(), 
+//						end.GetX(), end.GetY()))
+//					{
+//						m_toRemove.add(current);
+//					}
+//				}
 			}
-
-			if(m_entities.get(i) instanceof BlockingObject)
-			{
-				BlockingObject current = 
-					(BlockingObject)m_entities.get(i);
-
-				for(int j = 0; j < m_laserStarts.size();j++)
-				{
-					Player start = m_laserStarts.get(j);
-					Player end   = m_laserEnds.get(j);
-
-					if(current.LineIntersect(
-						start.GetX(), start.GetY(), 
-						end.GetX(), end.GetY()))
-					{
-						m_laserStartToRemove.add(start);
-						m_laserEndToRemove.add(end);
-					}
-				}
-			}
-
 		}
 
 		for(int i = 0; i < m_toRemove.size(); i++)
@@ -142,83 +116,133 @@ public class Game
 		}
 		m_toRemove.clear();
 
-		if(m_laserStartToRemove.size() != m_laserEndToRemove.size())
-		{
-			System.err.println("Error: Laser start/end removal list has fallen "
-					+ "out of alignment");
-//			for(int i = 0; i < m_laserStarts.size(); i++)
+//		for(int i = 0; i < m_entities.size(); i++)
+//		{
+//			if(m_entities.get(i) instanceof 
+//					DestroyableObject)
 //			{
-//				System.out.println(m_laserStarts.get(i));
+//				DestroyableObject current = 
+//					(DestroyableObject)m_entities.get(i);
+//
+//				for(int j = 0; j < m_laserStarts.size();j++)
+//				{
+//					Entity start = m_laserStarts.get(j);
+//					Entity end   = m_laserEnds.get(j);
+//
+//					if(current.LineIntersect(
+//						start.GetX(), start.GetY(), 
+//						end.GetX(), end.GetY()))
+//					{
+//						m_toRemove.add(current);
+//					}
+//				}
 //			}
-//			System.out.println();
-//			for(int i = 0; i < m_laserEnds.size(); i++)
+//
+//			if(m_entities.get(i) instanceof BlockingObject)
 //			{
-//				System.out.println(m_laserEnds.get(i));
+//				BlockingObject current = 
+//					(BlockingObject)m_entities.get(i);
+//
+//				for(int j = 0; j < m_laserStarts.size();j++)
+//				{
+//					Player start = m_laserStarts.get(j);
+//					Player end   = m_laserEnds.get(j);
+//
+//					if(current.LineIntersect(
+//						start.GetX(), start.GetY(), 
+//						end.GetX(), end.GetY()))
+//					{
+//						m_laserStartToRemove.add(start);
+//						m_laserEndToRemove.add(end);
+//					}
+//				}
 //			}
-
-
-			System.exit(1);
-		}
-
-		for(int i = 0; i < m_laserStartToRemove.size(); i++)
-		{
-			Player start = m_laserStartToRemove.get(i);
-			Player end   = m_laserEndToRemove.get(i);
-
-			start.SetIsActive(false);
-			end.SetIsActive(false);
-
-			int index = m_laserEnds.indexOf(start);
-			while(index != -1)
-			{
-				m_laserStarts.remove(index);
-				m_laserEnds.remove(index);
-				index = m_laserEnds.indexOf(start);
-			}
-			index = m_laserStarts.indexOf(start);
-			while(index != -1)
-			{
-				m_laserStarts.remove(index);
-				m_laserEnds.remove(index);
-				index = m_laserStarts.indexOf(start);
-			}
-
-			index = m_laserEnds.indexOf(end);
-			while(index != -1)
-			{
-				m_laserStarts.remove(index);
-				m_laserEnds.remove(index);
-				index = m_laserEnds.indexOf(end);
-			}
-			index = m_laserStarts.indexOf(end);
-			while(index != -1)
-			{
-				m_laserStarts.remove(index);
-				m_laserEnds.remove(index);
-				index = m_laserStarts.indexOf(end);
-			}
-		}
-		m_laserStartToRemove.clear();
-		m_laserEndToRemove.clear();
-
-		for(int i = 0; i < m_entities.size(); i++)
-		{
-			if(m_entities.get(i) instanceof Player)
-			{
-				Player current = 
-					(Player)m_entities.get(i);
-				if(current == m_mainPlayer)
-				{
-					continue;
-				}
-
-				if(!m_laserStarts.contains(current)
-					&& !m_laserEnds.contains(current))
-				{
-					current.SetIsActive(false);
-				}
-			}
-		}
+//
+//		}
+//
+//		for(int i = 0; i < m_toRemove.size(); i++)
+//		{
+//			m_entities.remove(m_toRemove.get(i));
+//		}
+//		m_toRemove.clear();
+//
+//		if(m_laserStartToRemove.size() != m_laserEndToRemove.size())
+//		{
+//			System.err.println("Error: Laser start/end removal list has fallen "
+//					+ "out of alignment");
+////			for(int i = 0; i < m_laserStarts.size(); i++)
+////			{
+////				System.out.println(m_laserStarts.get(i));
+////			}
+////			System.out.println();
+////			for(int i = 0; i < m_laserEnds.size(); i++)
+////			{
+////				System.out.println(m_laserEnds.get(i));
+////			}
+//
+//
+//			System.exit(1);
+//		}
+//
+//		for(int i = 0; i < m_laserStartToRemove.size(); i++)
+//		{
+//			Player start = m_laserStartToRemove.get(i);
+//			Player end   = m_laserEndToRemove.get(i);
+//
+//			start.SetIsActive(false);
+//			end.SetIsActive(false);
+//
+//			int index = m_laserEnds.indexOf(start);
+//			while(index != -1)
+//			{
+//				m_laserStarts.remove(index);
+//				m_laserEnds.remove(index);
+//				index = m_laserEnds.indexOf(start);
+//			}
+//			index = m_laserStarts.indexOf(start);
+//			while(index != -1)
+//			{
+//				m_laserStarts.remove(index);
+//				m_laserEnds.remove(index);
+//				index = m_laserStarts.indexOf(start);
+//			}
+//
+//			index = m_laserEnds.indexOf(end);
+//			while(index != -1)
+//			{
+//				m_laserStarts.remove(index);
+//				m_laserEnds.remove(index);
+//				index = m_laserEnds.indexOf(end);
+//			}
+//			index = m_laserStarts.indexOf(end);
+//			while(index != -1)
+//			{
+//				m_laserStarts.remove(index);
+//				m_laserEnds.remove(index);
+//				index = m_laserStarts.indexOf(end);
+//			}
+//		}
+//		m_laserStartToRemove.clear();
+//		m_laserEndToRemove.clear();
+//
+//		for(int i = 0; i < m_entities.size(); i++)
+//		{
+//			if(m_entities.get(i) instanceof Player)
+//			{
+//				Player current = 
+//					(Player)m_entities.get(i);
+//				if(current == m_mainPlayer)
+//				{
+//					continue;
+//				}
+//
+//				if(!m_laserStarts.contains(current)
+//					&& !m_laserEnds.contains(current))
+//				{
+//					current.SetIsActive(false);
+//				}
+//			}
+//		}
 	}
 
 	public void Render(RenderContext target)
@@ -226,22 +250,22 @@ public class Game
 		m_stars.UpdateAndRender(target, m_delta);
 		//target.Clear((byte)0x00);
 
-		if(m_laserStarts.size() != m_laserEnds.size())
-		{
-			System.err.println("Error: Laser start/end has fallen "
-					+ "out of alignment");
-			System.exit(1);
-		}
-
-		for(int i = 0; i < m_laserStarts.size(); i++)
-		{
-			Entity start = m_laserStarts.get(i);
-			Entity end   = m_laserEnds.get(i);
-
-			target.DrawLine(start.GetX(), start.GetY(),
-				   	end.GetX(), end.GetY(),
-				(byte)0x00, (byte)0x79, (byte)0xbf, (byte)0x10);
-		}
+//		if(m_laserStarts.size() != m_laserEnds.size())
+//		{
+//			System.err.println("Error: Laser start/end has fallen "
+//					+ "out of alignment");
+//			System.exit(1);
+//		}
+//
+//		for(int i = 0; i < m_laserStarts.size(); i++)
+//		{
+//			Entity start = m_laserStarts.get(i);
+//			Entity end   = m_laserEnds.get(i);
+//
+//			target.DrawLine(start.GetX(), start.GetY(),
+//				   	end.GetX(), end.GetY(),
+//				(byte)0x00, (byte)0x79, (byte)0xbf, (byte)0x10);
+//		}
 
 		for(int i = 0; i < m_entities.size(); i++)
 		{
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Player extends SpriteObject
 {
@@ -6,6 +7,61 @@ public class Player extends SpriteObject
 	private static final long DISABLE_LENGTH = 3 * SECOND;
 	private long         m_disableTime;
 	private boolean      m_isActive;
+	private final ArrayList<Player> m_children;
+
+	public void AddChild(Player other)
+	{
+		m_children.add(other);
+	}
+
+	public Player CheckCollisionRecursive(Player other)
+	{
+		if(SphereIntersect(other))
+		{
+			return this;
+		}
+
+		for(int i = 0; i < m_children.size(); i++)
+		{
+			Player result = m_children.get(i)
+				.CheckCollisionRecursive(other);
+			if(result != null)
+			{
+				return result;
+			}
+		}
+
+		return null;
+	}
+
+	public void CheckLaserCollision(Entity other,Player[] result)
+	{
+		float startX = GetX();
+		float startY = GetY();
+		for(int i = 0; i < m_children.size(); i++)
+		{	
+			if(!m_children.get(i).GetIsActive())
+			{
+				continue;
+			}
+
+			float endX = m_children.get(i).GetX();
+			float endY = m_children.get(i).GetY();
+			
+			if(other.LineIntersect(startX, startY, endX, endY))
+			{
+				result[0] = this;
+				result[1] = m_children.get(i);
+				return;
+			}
+
+			m_children.get(i).CheckLaserCollision(other, result);
+			if(result[0] != null)
+			{
+				return;
+			}
+		}
+	}
 
 	public boolean GetIsActive() { return m_isActive; }
 	public void SetIsActive(boolean val) 
@@ -31,6 +87,7 @@ public class Player extends SpriteObject
 				.ClearScreen((byte)0x00, (byte)0x00, 
 					(byte)0x00, (byte)0xFF));
 		m_isActive = isActive;
+		m_children = new ArrayList<Player>();
 		m_disableTime = 0;
 	}
 
@@ -57,5 +114,35 @@ public class Player extends SpriteObject
 				SetX(GetX() - delta * speed);
 			}
 		}
+
+		for(int i = 0; i < m_children.size(); i++)
+		{
+			m_children.get(i).Update(input, delta);
+		}
+	}
+
+	@Override
+	public void Render(RenderContext target)
+	{
+		if(m_isActive)
+		{
+			for(int i = 0; i < m_children.size(); i++)
+			{
+				if(m_children.get(i).GetIsActive())
+				{
+					target.DrawLine(GetX(), GetY(),
+							m_children.get(i).GetX(), 
+							m_children.get(i).GetY(),
+						(byte)0x00, 
+						(byte)0x79, (byte)0xbf, (byte)0x10);
+				}
+			}
+		}
+
+		for(int i = 0; i < m_children.size(); i++)
+		{
+			m_children.get(i).Render(target);
+		}
+		super.Render(target);
 	}
 }
